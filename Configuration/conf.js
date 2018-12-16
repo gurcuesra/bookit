f421ae33e98d874602f3cfa2052d151d3706aca6
gurcuesra <gurcuesra@gmail.com>
2018-12-16 13:29:17 -0800

    My Bookit Folders

---
+++ Configuration/conf.js
@@ -0,0 +1,43 @@
+let SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+var HtmlReporter = require('protractor-beautiful-reporter');
+
+exports.config ={
+    directConnect:true,
+
+    capabilities: {
+        browserName: 'chrome',
+        chromeOptions: {
+            args: [
+                '--start-fullscreen'
+            ]
+        }
+    },
+
+    specs:['../Tests/spec.js'],
+
+    onPrepare:function(){
+        // browser.manage().window().maximize();
+        jasmine.getEnv().addReporter(new SpecReporter({
+            displayFailuresSummary: true,
+            displayFailuredSpec: true,
+            displaySuiteNumber: true,
+            displaySpecDuration: true,
+            showstack: false
+        }));
+        // Add a screenshot reporter and store screenshots to `/tmp/screenshots`:
+        jasmine.getEnv().addReporter(new HtmlReporter({
+            baseDirectory: '../report/screenshots',
+            preserveDirectory: false,
+            screenshotsSubfolder: 'images',
+            jsonsSubfolder: 'jsons',
+            docName: 'Bookit-Project.html'
+        }).getJasmine2Reporter());
+    },
+
+    jasmineNodeOpts: {
+        showColors: true, 
+        defaultTimeoutInterval: 90000,    
+        print: function() {}   
+    }
+
+}
